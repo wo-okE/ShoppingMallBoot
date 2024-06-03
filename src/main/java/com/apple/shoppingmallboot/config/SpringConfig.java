dc8bba047506cdda36e03e87b7c2b58de910ce9e
seungwook <ymym455@naver.com>
2024-06-04 00:43:04 +0900

    ( HiddenHttpMethodFilter  )Configuration 추가
    View 와 연결하여
    PUT, DELETE Mapping을 사용하기 위해 생성

---
+++ src/main/java/com/apple/shoppingmallboot/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.apple.shoppingmallboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+@Configuration
+public class SpringConfig {
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        return hiddenHttpMethodFilter;
+    }
+}
