3363f5b78d3d693ce3b9cbad145dac64429acb1d
wo-okE <seugn97@gmail.com>
2024-06-03 09:59:38 +0900

    프로젝트 생성
    스프링 부트 3.2.3 버전의 프로젝트 생성 및
    .gitignore 파일 업데이트

---
+++ src/main/java/com/apple/shoppingmallboot/ShoppingMallBootApplication.java
@@ -0,0 +1,13 @@
+package com.apple.shoppingmallboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apple/shoppingmallboot/ShoppingMallBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.shoppingmallboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
