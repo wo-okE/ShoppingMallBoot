f1f5cc1b752c097276856d4d5776eaadab446785
wo-okE <seugn97@gmail.com>
2024-06-05 17:23:57 +0900

    상품 주문 Entity 추가

--- src/main/java/com/apple/shoppingmallboot/sales/Sales.java
+++ src/main/java/com/apple/shoppingmallboot/sales/Sales.java
@@ -1,2 +1,25 @@
-package com.apple.shoppingmallboot.sales;public class Sales {
+package com.apple.shoppingmallboot.sales;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Sales {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer count;
+    private Long memberId;
+    @CreationTimestamp
+    private LocalDateTime created;
 }
